cde737a678da5762a24b14dab9d8e57c70495f17
Anca6889 <guth.jeanmaxime@gmail.com>
2020-09-29 00:08:24 +0200

    VimCrypt~03!=.qï=Í<ÜübÌÂý/}ä©ÝVñ5¥ÞL	:/%¢¤q	TÛZ#ðòê4eÑܞà÷)ôé=[GÕ]8!N|©ËÁÿdtz¬V>©¹´ç(Ïäõ\ÂøÍÅ-ªÜjMßKü²·Þåehd.7T>°QA±ªårf<+\Öá1DÅtk;}AFíP |ûïí]LýKg<ÓAT¯Ý`¸iNÔ DiPdØXlLÑö Ǌ$aûÿ~µñÚD_ؒA8MÀ3yå¯`ö´O<6Vb1vR~i0 ႓kMïÍۛ¤Aã%îÏô;ڢ2*£úiú5Àp¬vÅÛDó퍛Ö![qNViUÇ

---
+++ listes13-16.py
@@ -0,0 +1,11 @@
+list_a = [4, 5]
+print (list_a)
+
+list_b = [6, 7, "huit", "neuf"]
+print(list_b[0])
+print(type(list_b[2]))
+
+list_c = [1, 2]
+list_d = [10, 11]
+list_e = [list_c + list_d]
+print(list_e)
